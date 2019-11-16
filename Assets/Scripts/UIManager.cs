0d0a0afa93af8f2d885ceaafff34f422eaa8dcfe
Mason Hayes (RIT Student) <mbh9947@ad.rit.edu>
2019-11-16 12:03:18 -0500

    Worked on more UI

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    //Variables
+    [SerializeField]
+    private GameObject mainMenu;
+    [SerializeField]
+    private GameObject gameMenu;
+    [SerializeField]
+    private GameObject gameOverMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu.SetActive(true);
+        gameMenu.SetActive(false);
+        gameOverMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlayGame()
+    {
+        mainMenu.SetActive(false);
+        gameMenu.SetActive(true);
+        gameOverMenu.SetActive(false);
+    }
+
+}
