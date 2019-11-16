3118cd9d1823d59b98585186a7f075f2265d03dd
Mason Hayes (RIT Student) <mbh9947@ad.rit.edu>
2019-11-15 20:19:52 -0500

    Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Constantly adjusts position
+        position.z += (speed * Time.deltaTime);
+        gameObject.transform.position = position;
+    }
+}
